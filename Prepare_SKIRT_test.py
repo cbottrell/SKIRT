39579f3addc5041852be304736ed45ed28e18b75
Connor Bottrell <connor.bottrell@gmail.com>
2021-05-24 17:53:13 +0900

    Testing files.

---
+++ Prepare_SKIRT_test.py
@@ -0,0 +1,7 @@
+import sys
+from Run_SKIRT_test import prepare_only
+
+if __name__=='__main__':
+    
+    print(sys.argv)
+    prepare_only(sys.argv)
---
+++ salloc_cpu.sh
@@ -0,0 +1,1 @@
+qsub -l select=1:ncpus=48 -l walltime=3:0:0 -l pmem=3gb -q mini2 -I
